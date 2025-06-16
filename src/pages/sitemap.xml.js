712168068c64a08f271726d4b9150ef0cb6b72e5
Brandon Palmeros <brandonpaldom@gmail.com>
2025-06-16 00:17:23 -0600

    refactor: standardize quotation marks and remove unused blog styles

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,11 +1,11 @@
-import { getCollection } from 'astro:content';
+import { getCollection } from "astro:content";
 
 export async function GET({ site }) {
-  const baseUrl = site || 'https://brandonpalmeros.dev/';
+  const baseUrl = site || "https://brandonpalmeros.dev/";
   // Get all blog posts
-  const posts = await getCollection('blog');
-  
-  const blogUrls = posts.map(post => ({
+  const posts = await getCollection("blog");
+
+  const blogUrls = posts.map((post) => ({
     url: `${baseUrl}blog/${post.slug}`,
     lastmod: new Date(post.data.pubDate).toISOString(),
   }));
@@ -20,16 +20,20 @@ export async function GET({ site }) {
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${allUrls.map(({ url, lastmod }) => `
+${allUrls
+  .map(
+    ({ url, lastmod }) => `
   <url>
     <loc>${url}</loc>
     <lastmod>${lastmod}</lastmod>
-  </url>`).join('')}
+  </url>`,
+  )
+  .join("")}
 </urlset>`;
 
   return new Response(sitemap, {
     headers: {
-      'Content-Type': 'application/xml',
+      "Content-Type": "application/xml",
     },
   });
 }
